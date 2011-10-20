96c3b2ec1eb02f55405f483c6673df4d2e87a00a
Philipp Comans <github@volton.otherinbox.com>
2011-10-20 17:07:21 +0200

    Renamed binaries

--- test/test_blackbox.rb
+++ test/test_blackbox.rb
@@ -4,14 +4,14 @@ require 'tmpdir'
 class BlackBoxTest < Test::Unit::TestCase
   def test_without_parameters
     #This test does not make a whole lot of sense...
-    result = %x[bin/kingdom-assignment]
+    result = %x[bin/phyta-assign]
     expected = "Invalid arguments, see --help for more information."
     assert_equal expected.strip, result.strip
   end
   
   def test_small
     Dir.mktmpdir do |dir|
-      %x[bin/kingdom-assignment -i test/data/in_3.xml -o #{dir}/out_3.csv]
+      %x[bin/phyta-assign -i test/data/in_3.xml -o #{dir}/out_3.csv]
       result = File.open("#{dir}/out_3.csv").read
       target = File.open("test/data/target_3.csv").read
 
@@ -24,7 +24,7 @@ class BlackBoxTest < Test::Unit::TestCase
 
   def test_medium
     Dir.mktmpdir do |dir|
-      %x[bin/kingdom-assignment -i test/data/in_medium.xml -o #{dir}/out_medium.csv]
+      %x[bin/phyta-assign -i test/data/in_medium.xml -o #{dir}/out_medium.csv]
       result = File.open("#{dir}/out_medium.csv").read
       target = File.open("test/data/target_medium.csv").read
 
