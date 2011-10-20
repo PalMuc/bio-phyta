e44ad20b8782b184539a6311f59e878de255e4ed
Philipp Comans <github@volton.otherinbox.com>
2011-10-20 16:41:53 +0200

    Passing tests again

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,6 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'bio-phyta'
 
 class Test::Unit::TestCase
 end
